70468673467493eb8b8142e02a2b72ed9debe7e3
johnyob <johnyob132@gmail.com>
2018-07-16 14:53:21 +0100

    Create parseWikipedia.js. Used to scrap algorithms / data structures from wikipedia. Uses a heuristic algorithm to identify whether a given page is an algorithm or not. Note to self; this script is not stable (timeout errors occur frequently).

---
+++ algorithms/parseWikipedia.js
@@ -0,0 +1,96 @@
+const wikipedia = require("mediawiki-api-js");
+const database = require("./db");
+const ddl = require("./ddl");
+
+const MAX_DEPTH = 3;
+const HEURISTIC_IDENTIFIERS = [ //heuristic
+  "algorithm", "Algorithm",
+  "data structure", "Data structure",
+  "abstract data type",
+  "Abstract data type",
+  "tree", "Tree",
+  "search", "Search",
+  "sort", "Sort"
+];
+
+/**
+ * Helper Functions
+ */
+var isAlgorithm = (page) => HEURISTIC_IDENTIFIERS.filter(x => page.includes(x)).length > 0;
+
+var isAlgorithmCategory = page => page.includes("Category:") && isAlgorithm(page);
+
+var isAlgorithmPage = (page) => {
+  return {
+    page: page,
+    condition: page.categories.then(categories => categories.filter(
+      isAlgorithm
+    ).length > 0).then(condition => page.content.then(
+      summary => condition && isAlgorithm(summary)
+    ))
+  };
+};
+
+
+/**
+ * Functions
+ */
+
+var handleCategory = (id, category) => database.connect().then(db => db.insert(
+    "INSERT OR IGNORE INTO Category(CategoryTitle) "
+  + "VALUES (?);", [category]
+)).then(db => db.insert(
+    "INSERT OR IGNORE INTO CategoryLink "
+  + "VALUES (?, (SELECT CategoryID FROM "
+  + "Category WHERE CategoryTitle = ?) "
+  + ");", [id, category]
+)).then(db => db.delete(
+    "DELETE FROM CategoryLink "
+  + "WHERE CategoryID IS NULL;", []
+)).then(db => db.close());
+
+var parsePage = page => page.summary.then(summary => database.connect().then(db => db.insert(
+    "INSERT OR IGNORE INTO Algorithm "
+  + "VALUES (?, ?, ?);",
+    [page.id, page.title, summary]
+)).then(db => {
+  page.categories.then(categories => categories.filter(isAlgorithmCategory).forEach(
+    category => handleCategory(page.id, category)
+  ));
+  return db;
+}).then(db => db.close()));
+
+var parseCategory = (category, depth, visited) => {
+  console.log("[ALERT] Depth: " + depth);
+  if (depth >= MAX_DEPTH) {
+    return;
+  }
+
+  wikipedia().pagesInCategory(category).then(pages => pages.forEach(page => {
+    console.log("[INFO] Page in pages: " + page);
+    if (isAlgorithmCategory(page)) {
+      console.log("[INFO] Category: " + category);
+
+      database.connect().then(db => db.insert(
+          "INSERT OR IGNORE INTO Category(CategoryTitle) "
+        + "VALUES (?);", [page]
+      )).then(db => db.close());
+      parseCategory(page, depth + 1, visited);
+    } else if (!page.includes("Category:") && !visited.includes(page)) {
+      console.log("[INFO] Page: " + page);
+
+      wikipedia().page(page).then(isAlgorithmPage).then(result => result.condition.then(
+        condition => condition
+          ? result.page
+          : (() => {throw new Error("Page isn't an algorithm")})()
+      ).then(parsePage).catch(error => console.log("[ALERT] Page: " + page + " isn't an algorithm.")));
+    }
+  }));
+};
+
+ddl();
+var category = "Category:Sorting_algorithms";
+//used to reduce timeout
+database.connect().then(db => db.query(
+  "SELECT AlgorithmTitle FROM Algorithm;", []
+)).then(visited => parseCategory(category, 0, visited));
