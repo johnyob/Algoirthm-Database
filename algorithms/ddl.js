1a6f3b571335cafbf58e15695588c8d6e2b04fac
johnyob <johnyob132@gmail.com>
2018-07-16 14:51:54 +0100

    Create ddl.js. Data Definition Language used to define tables in Algorithms.db. Executed upon startup for wikipedia scraper.

---
+++ algorithms/ddl.js
@@ -0,0 +1,36 @@
+const database = require("./db");
+var createTables = () => {
+  var create = (sql) => {
+    database.connect().then(db => db.insert(sql)).then(db => db.close()).catch(error => console.log(error));
+  }
+
+  create(
+      "CREATE TABLE IF NOT EXISTS Algorithm ("
+    + "  AlgorithmID INTEGER PRIMARY KEY AUTOINCREMENT,"
+    + "  AlgorithmTitle TEXT,"
+    + "  AlgorithmSummary TEXT"
+    + ");"
+  );
+
+  create(
+      "CREATE TABLE IF NOT EXISTS Category ("
+    + "  CategoryID INTEGER PRIMARY KEY AUTOINCREMENT,"
+    + "  CategoryTitle TEXT,"
+    + "  CONSTRAINT category_unique UNIQUE (CategoryTitle)"
+    + ");"
+  );
+
+  create(
+      "CREATE TABLE IF NOT EXISTS CategoryLink ("
+    + "  AlgorithmID INTEGER,"
+    + "  CategoryID INTEGER,"
+    + "  PRIMARY KEY (AlgorithmID, CategoryID),"
+    + "  FOREIGN KEY (AlgorithmID) REFERENCES Algorithm (AlgorithmID),"
+    + "  FOREIGN KEY (CategoryID) REFERENCES Category (CategoryID)"
+    + ");"
+  );
+};
+
+module.exports = () => {
+  createTables();
+}
