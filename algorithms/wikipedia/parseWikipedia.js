3a493555eb91924d817882d82e57ec27d642ed1e
johnyob <johnyob132@gmail.com>
2018-07-17 14:52:31 +0100

    Update parseWikipedia.js. Added JSDoc

--- algorithms/wikipedia/parseWikipedia.js
+++ algorithms/wikipedia/parseWikipedia.js
@@ -2,10 +2,23 @@ const wikipedia = require("mediawiki-api-js");
 const database = require("../db");
 const ddl = require("./ddl");
 
-const MAX_DEPTH = 3;
 const DB = "./Algorithms.db";
 
-const HEURISTIC_IDENTIFIERS = [ //heuristic
+/**
+ * Constant that contains the maximum recursion depth for the parseCategory
+ * function
+ * @const {Number} MAX_DEPTH
+ */
+
+const MAX_DEPTH = 3;
+
+/**
+ * Constant that contains the strings that are matched to a page. Used to determine
+ * whether a page is an algorithm / data structure.
+ * @const {Array} HEURISTIC_IDENTIFIERS
+ */
+
+const HEURISTIC_IDENTIFIERS = [
   "algorithm", "Algorithm",
   "data structure", "Data structure",
   "abstract data type",
@@ -16,53 +29,100 @@ const HEURISTIC_IDENTIFIERS = [ //heuristic
 ];
 
 /**
- * Helper Functions
+ * (Heuristic) Used to determine whether a page is an algorithm / data structure
+ * @function isAlgorithm
+ * @param {String} page
+ * @return {Boolean}
  */
-var isAlgorithm = (page) => HEURISTIC_IDENTIFIERS.filter(x => page.includes(x)).length > 0;
 
-var isAlgorithmCategory = page => page.includes("Category:") && isAlgorithm(page);
+function isAlgorithm(page) {
+  return HEURISTIC_IDENTIFIERS.filter(x => page.includes(x)).length > 0;
+}
 
-var isAlgorithmPage = (page) => {
+/**
+ * (Heuristic) Used to determine whether a category contains algorithm pages
+ * @function isAlgorithmCategory
+ * @param {String} category
+ * @return {Boolean}
+ */
+
+function isAlgorithmCategory(category) {
+  return page.includes("Category:") && isAlgorithm(category);
+}
+
+/**
+ * (Heuristic) Used to determine whether a page is an algorithm / data structure
+ * by checking the categories that the page belongs too and the contents of the
+ * page
+ * @function isAlgorithmPage
+ * @param {String} page
+ * @return {Object} contains {wikipedia.Page} page and {Promise} condition which
+ * resolves to a boolean
+ */
+
+function isAlgorithmPage(page) {
   return {
     page: page,
     condition: page.categories.then(categories => categories.filter(
       isAlgorithm
     ).length > 0).then(condition => page.content.then(
-      summary => condition && isAlgorithm(summary)
+      content => condition && isAlgorithm(content)
     ))
   };
-};
-
+}
 
 /**
- * Functions
+ * Inserts category into Category table if it does not yet exist, then
+ * inserts the link between the AlgorithmID and CategoryID and then deletes
+ * any links where the CategoryID IS NULL
+ * @function handleCategory
+ * @param {Number} id
+ * @param {String} category
  */
 
-var handleCategory = (id, category) => database.connect(DB).then(db => db.insert(
-    "INSERT OR IGNORE INTO Category(CategoryTitle) "
-  + "VALUES (?);", [category]
-)).then(db => db.insert(
-    "INSERT OR IGNORE INTO CategoryLink "
-  + "VALUES (?, (SELECT CategoryID FROM "
-  + "Category WHERE CategoryTitle = ?) "
-  + ");", [id, category]
-)).then(db => db.delete(
-    "DELETE FROM CategoryLink "
-  + "WHERE CategoryID IS NULL;", []
-)).then(db => db.close());
+function handleCategory(id, category) {
+  database.connect(DB).then(db => db.insert(
+      "INSERT OR IGNORE INTO Category(CategoryTitle) "
+    + "VALUES (?);", [category]
+  )).then(db => db.insert(
+      "INSERT OR IGNORE INTO CategoryLink "
+    + "VALUES (?, (SELECT CategoryID FROM "
+    + "Category WHERE CategoryTitle = ?) "
+    + ");", [id, category]
+  )).then(db => db.delete(
+      "DELETE FROM CategoryLink "
+    + "WHERE CategoryID IS NULL;", []
+  )).then(db => db.close());
+}
 
-var parsePage = page => page.summary.then(summary => database.connect(DB).then(db => db.insert(
-    "INSERT OR IGNORE INTO Algorithm "
-  + "VALUES (?, ?, ?);",
-    [page.id, page.title, summary]
-)).then(db => {
-  page.categories.then(categories => categories.filter(isAlgorithmCategory).forEach(
-    category => handleCategory(page.id, category)
-  ));
-  return db;
-}).then(db => db.close()));
+/**
+ * Inserts algorithm in Algorithm if it does not yet exist, then handle any
+ * categories that the algorithm belongs too
+ * @function parsePage
+ * @param {wikipedia.Page} page
+ */
 
-var parseCategory = (category, depth, visited) => {
+function parsePage(page) {
+  page.summary.then(summary => database.connect(DB).then(db => db.insert(
+      "INSERT OR IGNORE INTO Algorithm "
+    + "VALUES (?, ?, ?);",
+      [page.id, page.title, summary]
+  )).then(db => {
+    page.categories.then(categories => categories.filter(isAlgorithmCategory).forEach(
+      category => handleCategory(page.id, category)
+    ));
+    return db;
+  }).then(db => db.close()));
+}
+
+/**
+ * @function parseCategory
+ * @param {String} category (<category> ::= Category:<identifier>;)
+ * @param {Number} depth current recursive depth
+ * @param {Array} visited contains list of algorithms stored in the database
+ */
+
+function parseCategory(category, depth, visited) {
   console.log("[ALERT] Depth: " + depth);
   if (depth >= MAX_DEPTH) {
     return;
@@ -88,12 +148,10 @@ var parseCategory = (category, depth, visited) => {
       ).then(parsePage).catch(error => console.log("[ALERT] Page: " + page + " isn't an algorithm.")));
     }
   }));
-};
+}
 
 ddl();
-
-var category = "Category:Sorting_algorithms";
-//used to reduce timeout
+const CATEGORY = "Category:Sorting_algorithms";
 database.connect(DB).then(db => db.query(
   "SELECT AlgorithmTitle FROM Algorithm;", []
-)).then(visited => parseCategory(category, 0, visited));
+)).then(visited => parseCategory(CATEGORY, 0, visited));
